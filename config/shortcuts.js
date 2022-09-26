43bd1752508d430f726d9f2ceb5f6b3a1ab961a3
FeBagnato <felipebag@protonmail.com>
2022-09-26 16:25:38 -0300

    Adding website shortcuts
    data is a array that stores the website's name, link and favicon.
    This informations will be used to display the shortcuts in the
    new tabs

---
+++ config/shortcuts.js
@@ -0,0 +1,4 @@
+data = '[' +
+'{"name": "Google","link": "google.com", "favicon": "http://www.google.com/s2/favicons?domain=google.com"},' +
+'{"name": "Youtube", "link": "youtube.com", "favicon": "http://www.google.com/s2/favicons?domain=youtube.com"}' +
+']'
