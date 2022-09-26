df8ea3db37d423eefba545d6a733b67d07f7907b
FeBagnato <felipebag@protonmail.com>
2022-09-26 11:34:38 -0300

    :sparkles: Choose a image randomly

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function setBackgroundImage(){
-    document.body.setAttribute('style', '--img:url("images/7.webp")')
+    let total_images = 6
+    let random_image = Math.round(Math.random() * (total_images - 1) + 1)
+    
+    document.body.setAttribute('style', '--img:url("images/' 
+    + random_image + '.webp")')
 }
 
 setBackgroundImage()
